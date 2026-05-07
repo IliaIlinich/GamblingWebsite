433735dc28547db858bd9116ec02fe30b0b1a05b
IliaIlinich <ilya.ilinich@gmail.com>
2026-05-07 12:08:09 +0100

    The wheel of fortune is now rigged to give only zero; Wheel of fortune no longer can be spun again whilst it is spinning; Rotation angle being reset every button press has been fixed;

--- src/wheel-of-fortune.js
+++ src/wheel-of-fortune.js
@@ -1,3 +1,47 @@
+function checkResult(previousEndDegree) {
+  let result = "";
+
+  switch (true) {
+    case previousEndDegree > 15 && previousEndDegree <= 45:
+      result = "lost it all";
+      break;
+    case previousEndDegree > 45 && previousEndDegree <= 75:
+      result = "5£";
+      break;
+    case previousEndDegree > 75 && previousEndDegree <= 105:
+      result = "lost it all";
+      break;
+    case previousEndDegree > 105 && previousEndDegree <= 135:
+      result = "lost it all";
+      break;
+    case previousEndDegree > 135 && previousEndDegree <= 165:
+      result = "5£";
+      break;
+    case previousEndDegree > 165 && previousEndDegree <= 195:
+      result = "lost it all";
+      break;
+    case previousEndDegree > 195 && previousEndDegree <= 225:
+      result = "10£";
+      break;
+    case previousEndDegree > 225 && previousEndDegree <= 255:
+      result = "lost it all";
+      break;
+    case previousEndDegree > 255 && previousEndDegree <= 285:
+      result = "jackpot";
+      break;
+    case previousEndDegree > 285 && previousEndDegree <= 315:
+      result = "jackpot";
+      break;
+    case previousEndDegree > 315 && previousEndDegree <= 345:
+      result = "zero";
+      break;
+    case previousEndDegree > 345 || previousEndDegree <= 15: // Fixed logic here
+      result = "10£";
+      break;
+  }
+  return result;
+}
+
 function wheelOfFortune(selector) {
   const node = document.querySelector(selector);
   if (!node) return;
@@ -8,28 +52,43 @@ function wheelOfFortune(selector) {
   let previousEndDegree = 0;
 
   spin.addEventListener('click', () => {
-    if (animation) {
-      animation.cancel(); // Reset the animation if it already exists
+    if (!animation) {
+      let randomAdditionalDegrees = Math.random() * 360 + 1800;
+      let newEndDegree = previousEndDegree + randomAdditionalDegrees;
+      while (previousEndDegree >= 360) {
+        previousEndDegree -= 360;
+      }
+      while (newEndDegree >= 360) {
+        newEndDegree -= 360;
+      }
+      console.log(previousEndDegree, newEndDegree);
+
+      while (checkResult(newEndDegree) !== "lost it all") {
+        randomAdditionalDegrees = Math.random() * 360 + 1800;
+        newEndDegree = previousEndDegree + randomAdditionalDegrees;
+        while (newEndDegree >= 360) {
+          newEndDegree -= 360;
+        }
+      }
+      console.log(previousEndDegree, newEndDegree);
+      animation = wheel.animate(
+        [
+          { transform: `rotate(${previousEndDegree}deg)` },
+          { transform: `rotate(${newEndDegree + (360 * 4)}deg)` },
+        ],
+        {
+          duration: 4000,
+          direction: 'normal',
+          easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
+          fill: 'forwards',
+          iterations: 1,
+        },
+      );
+      setTimeout(function() { animation = false; }, 4100);
+      setTimeout(function() { previousEndDegree = newEndDegree; }, 4100);
+    } else {
+      console.log("The wheel is already spinning")
     }
-
-    const randomAdditionalDegrees = Math.random() * 360 + 1800;
-    const newEndDegree = previousEndDegree + randomAdditionalDegrees;
-
-    animation = wheel.animate(
-      [
-        { transform: `rotate(${previousEndDegree}deg)` },
-        { transform: `rotate(${newEndDegree}deg)` },
-      ],
-      {
-        duration: 4000,
-        direction: 'normal',
-        easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
-        fill: 'forwards',
-        iterations: 1,
-      },
-    );
-
-    previousEndDegree = newEndDegree;
   });
 }
 
